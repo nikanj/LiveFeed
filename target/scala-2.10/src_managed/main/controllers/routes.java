d1b380a6e06b69978682f5bf89131287bdfc2fe2
nikanjgupta <nikanjgupta@gmail.com>
2014-05-21 21:49:25 +0200

    Commit With all files

---
+++ target/scala-2.10/src_managed/main/controllers/routes.java
@@ -0,0 +1,19 @@
+// @SOURCE:D:/Master's Germany/Sem 4 subs/SEBA/Play app's/LiveFeed/conf/routes
+// @HASH:9c24f09449355737d0bfbe9ca53280febbd777c7
+// @DATE:Wed May 21 20:11:50 CEST 2014
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+public static class javascript {
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+}
+public static class ref {
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+}
+}
+          
