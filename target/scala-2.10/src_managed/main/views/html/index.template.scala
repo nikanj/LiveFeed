d851397da9fef8d0e549dd49742ce8d824db12ef
Ponnulakshmi <sponnulakshmi@yahoo.com>
2014-05-29 20:10:46 +0200

    HomePage

--- target/scala-2.10/src_managed/main/views/html/index.template.scala
+++ target/scala-2.10/src_managed/main/views/html/index.template.scala
@@ -28,7 +28,7 @@ object index extends BaseScalaTemplate[play.api.templates.HtmlFormat.Appendable,
 
 Seq[Any](format.raw/*1.19*/("""
 
-"""),_display_(Seq[Any](/*3.2*/main("Welcome to Play")/*3.25*/ {_display_(Seq[Any](format.raw/*3.27*/("""
+"""),_display_(Seq[Any](/*3.2*/main("Live Feed")/*3.19*/ {_display_(Seq[Any](format.raw/*3.21*/("""
 
     """),_display_(Seq[Any](/*5.6*/play20/*5.12*/.welcome(message, style = "Java"))),format.raw/*5.45*/("""
 
@@ -45,10 +45,10 @@ Seq[Any](format.raw/*1.19*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Thu May 29 17:48:19 CEST 2014
+                    DATE: Thu May 29 20:09:30 CEST 2014
                     SOURCE: /Users/roshinisachithanandan/git/LiveFeed/app/views/index.scala.html
-                    HASH: c1c7a8094ff6fdf7b189be1a11ff9a14918bbdfd
-                    MATRIX: 774->1|885->18|922->21|953->44|992->46|1033->53|1047->59|1101->92|1134->95
+                    HASH: 37dbae2988af98bc50c6c53d04df7f64fe2593cc
+                    MATRIX: 774->1|885->18|922->21|947->38|986->40|1027->47|1041->53|1095->86|1128->89
                     LINES: 26->1|29->1|31->3|31->3|31->3|33->5|33->5|33->5|35->7
                     -- GENERATED --
                 */
--- target/scala-2.10/src_managed/main/views/html/main.template.scala
+++ target/scala-2.10/src_managed/main/views/html/main.template.scala
@@ -35,10 +35,20 @@ Seq[Any](format.raw/*1.32*/("""
         <title>"""),_display_(Seq[Any](/*7.17*/title)),format.raw/*7.22*/("""</title>
         <link rel="stylesheet" media="screen" href=""""),_display_(Seq[Any](/*8.54*/routes/*8.60*/.Assets.at("stylesheets/main.css"))),format.raw/*8.94*/("""">
         <link rel="shortcut icon" type="image/png" href=""""),_display_(Seq[Any](/*9.59*/routes/*9.65*/.Assets.at("images/favicon.png"))),format.raw/*9.97*/("""">
-        <script src=""""),_display_(Seq[Any](/*10.23*/routes/*10.29*/.Assets.at("javascripts/jquery-1.9.0.min.js"))),format.raw/*10.74*/("""" type="text/javascript"></script>
+        <link rel="stylesheet" type="text/css" href=""""),_display_(Seq[Any](/*10.55*/routes/*10.61*/.Assets.at("stylesheets/bootstrap/css/bootstrap.css"))),format.raw/*10.114*/("""">
+        <script src=""""),_display_(Seq[Any](/*11.23*/routes/*11.29*/.Assets.at("javascripts/jquery-1.9.0.min.js"))),format.raw/*11.74*/("""" type="text/javascript"></script>
     </head>
     <body>
-        """),_display_(Seq[Any](/*13.10*/content)),format.raw/*13.17*/("""
+        <div class="page-header" style="padding-bottom: 0px; margin-top:5px;">
+    	<img src=""""),_display_(Seq[Any](/*15.17*/routes/*15.23*/.Assets.at("images/logo.png"))),format.raw/*15.52*/("""" height="100px" width="300px"alt="Live Feed"> 
+    </div> 
+       <header class="navbar navbar-static-top bs-docs-nav"  id="top" role="banner">
+  		<div class="well btn-group" style="margin-left:400px;margin-top:100px">
+  		<p><a type="button" class="btn btn-primary btn-lg btn-block" href=".." style="width:200px;">Tutors</a></p>
+  		<p><a type="button" class="btn btn-primary btn-lg btn-block" href=".." style="width:200px;">Students</a></p>
+  		</div>
+	</header>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     </body>
 </html>
 """))}
@@ -53,11 +63,11 @@ Seq[Any](format.raw/*1.32*/("""
 }
                 /*
                     -- GENERATED --
-                    DATE: Thu May 29 17:48:19 CEST 2014
+                    DATE: Thu May 29 20:09:30 CEST 2014
                     SOURCE: /Users/roshinisachithanandan/git/LiveFeed/app/views/main.scala.html
-                    HASH: 269b46d0510c6906d1a4d27ca715f76cc91ac485
-                    MATRIX: 778->1|902->31|990->84|1016->89|1113->151|1127->157|1182->191|1278->252|1292->258|1345->290|1406->315|1421->321|1488->366|1591->433|1620->440
-                    LINES: 26->1|29->1|35->7|35->7|36->8|36->8|36->8|37->9|37->9|37->9|38->10|38->10|38->10|41->13|41->13
+                    HASH: 83b9da540d787a97f46c2d82a54ed339f28cad93
+                    MATRIX: 778->1|902->31|990->84|1016->89|1113->151|1127->157|1182->191|1278->252|1292->258|1345->290|1438->347|1453->353|1529->406|1590->431|1605->437|1672->482|1861->635|1876->641|1927->670
+                    LINES: 26->1|29->1|35->7|35->7|36->8|36->8|36->8|37->9|37->9|37->9|38->10|38->10|38->10|39->11|39->11|39->11|43->15|43->15|43->15
                     -- GENERATED --
                 */
             
