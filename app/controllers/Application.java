b31af2d95f3f72e0419e7b2ccddaf24c08e137d9
Ponnulakshmi <sponnulakshmi@yahoo.com>
2014-05-29 11:54:10 +0200

    initial database connection

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,14 +1,42 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-import views.html.*;
+import javax.sql.DataSource;
+
+import play.db.DB;
+import play.mvc.Controller;
+import play.mvc.Result;
+import views.html.index;
 
 public class Application extends Controller {
 
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	public static Result index() throws SQLException {
+		DataSource ds = DB.getDataSource();
+		java.sql.Connection conn = DB.getConnection();
+		java.sql.Statement stmt = conn.createStatement();
+		String sql;
+		sql = "SELECT ID, ProfName, Password FROM login";
+		ResultSet rs = stmt.executeQuery(sql);
+
+		// STEP 5: Extract data from result set
+		while (rs.next()) {
+			// Retrieve by column name
+			int id = rs.getInt("ID");
+			String name = rs.getString("ProfName");
+			String password = rs.getString("Password");
+
+			// Display values
+			System.out.print("ID: " + id);
+			System.out.print(", ProfName: " + name);
+			System.out.println(", Password: " + password);
+		}
+		// STEP 6: Clean-up environment
+		rs.close();
+		stmt.close();
+		conn.close();
+		return ok(index.render("Your new application is ready."));
+	}
 
 }
--- build.sbt
+++ build.sbt
@@ -5,7 +5,11 @@ version := "1.0-SNAPSHOT"
 libraryDependencies ++= Seq(
   javaJdbc,
   javaEbean,
-  cache
+  cache,
+  jdbc,
+  "org.hibernate" % "hibernate-entitymanager" % "3.6.9.Final",
+  "mysql" % "mysql-connector-java" % "5.1.27"
 )     
+
 
 play.Project.playJavaSettings
