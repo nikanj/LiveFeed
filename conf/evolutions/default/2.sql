f25cc86d406853b24a68b6677816c51f4fb2b5bc
Ponnulakshmi <sponnulakshmi@yahoo.com>
2014-05-29 17:44:48 +0200

    sql files updated

--- conf/evolutions/default/2.sql
+++ conf/evolutions/default/2.sql
@@ -2,8 +2,8 @@
 
 # --- !Ups
 
-Insert into login (ID, ProfName, Password) values (1,'bruegge', 'test1');
-Insert into login (ID, ProfName, Password) values (2,'jacobson', 'test2');
+Insert into login (ID_prof, ProfName, Password) values (1,'bruegge', 'test1');
+Insert into login (ID_prof, ProfName, Password) values (2,'jacobson', 'test2');
 
 
 # --- !Downs
